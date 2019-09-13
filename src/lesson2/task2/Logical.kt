b0cd62cc003ce45d6dc59d0d2174680068d57510
donebd <dinil.bd@mail.ru>
2019-09-13 23:35:16 +0300

    lesson 2 question

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -19,9 +19,7 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean {
-    return number % 10 + number % 100 / 10 == number / 1000 + number % 1000 / 100
-}
+fun isNumberHappy(number: Int): Boolean = number % 10 + number % 100 / 10 == number / 1000 + number % 1000 / 100
 
 /**
  * Простая
@@ -30,9 +28,7 @@ fun isNumberHappy(number: Int): Boolean {
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
-    return x1 == x2 || y1 == y2 || abs(x1 - x2) == abs(y1 - y2)
-}
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = x1 == x2 || y1 == y2 || abs(x1 - x2) == abs(y1 - y2)
 
 
 /**
@@ -41,7 +37,7 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int = when (month){
+fun daysInMonth(month: Int, year: Int): Int = when (month) {
     1, 3, 5, 7, 8, 10, 12 -> 31
     2 -> if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) 29 else 28
     else -> 30
@@ -69,4 +65,14 @@ fun circleInside(
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean {
+    return when {
+        a <= r -> b <= s || c <= s
+        a <= s -> b <= r || c <= r
+        b <= r -> a <= s || c <= s
+        b <= s -> a <= r || c <= r
+        c <= r -> a <= s || b <= s
+        c <= s -> a <= r || b <= r
+        else -> false
+    }
+}
