40986a224dadf19e934425f95bfe049db33e0512
done_bd <dinil.bd@mail.ru>
2019-10-18 23:03:19 +0300

    Lesson 5 in progress

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -83,7 +83,6 @@ fun buildWordSet(text: List<String>): MutableSet<String> {
     return res
 }
 
-
 /**
  * Простая
  *
@@ -308,6 +307,7 @@ fun propagateHandshakes(friends: Map<String, Set<String>>): Map<String, Set<Stri
  *   findSumOfTwo(listOf(1, 2, 3), 6) -> Pair(-1, -1)
  */
 fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int> {
+    if (number == 0) return Pair(-1, -1)
     for (i in 0..list.size - 2)
         for (j in i until list.size) if (list[i] + list[j] == number) return Pair(i, j)
     return Pair(-1, -1)
