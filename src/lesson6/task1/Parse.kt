b6800660a01aa35428946f308cba139c7ddd6152
done_bd <dinil.bd@mail.ru>
2019-11-07 18:52:55 +0300

    6 Lesson some fix

--- src/lesson6/task1/Parse.kt
+++ src/lesson6/task1/Parse.kt
@@ -71,30 +71,26 @@ fun main() {
  * входными данными.
  */
 fun dateStrToDigit(str: String): String {
-    try {
-        val date = str.split(" ")
-        if (date.size != 3) return ""
-        val day = date[0].toInt()
-        val month = when (date[1]) {
-            "января" -> 1
-            "февраля" -> 2
-            "марта" -> 3
-            "апреля" -> 4
-            "мая" -> 5
-            "июня" -> 6
-            "июля" -> 7
-            "августа" -> 8
-            "сентября" -> 9
-            "октября" -> 10
-            "ноября" -> 11
-            "декабря" -> 12
-            else -> 0
-        }
-        if (month == 0 || daysInMonth(month, date[2].toInt()) < day) return ""
-        return String.format("%02d.%02d.%s", day, month, date[2])
-    } catch (e: Exception) {
-        return ""
+    val date = str.split(" ")
+    if (date.size != 3 || date[0].toIntOrNull() == null || date[2].toIntOrNull() == null) return ""
+    val day = date[0].toInt()
+    val month = when (date[1]) {
+        "января" -> 1
+        "февраля" -> 2
+        "марта" -> 3
+        "апреля" -> 4
+        "мая" -> 5
+        "июня" -> 6
+        "июля" -> 7
+        "августа" -> 8
+        "сентября" -> 9
+        "октября" -> 10
+        "ноября" -> 11
+        "декабря" -> 12
+        else -> 0
     }
+    if (month == 0 || daysInMonth(month, date[2].toInt()) < day) return ""
+    return String.format("%02d.%02d.%s", day, month, date[2])
 }
 
 /**
@@ -108,30 +104,26 @@ fun dateStrToDigit(str: String): String {
  * входными данными.
  */
 fun dateDigitToStr(digital: String): String {
-    try {
-        val date = digital.split(".")
-        if (date.size != 3) return ""
-        val day = date[0].toInt()
-        val month = when (date[1]) {
-            "01" -> "января"
-            "02" -> "февраля"
-            "03" -> "марта"
-            "04" -> "апреля"
-            "05" -> "мая"
-            "06" -> "июня"
-            "07" -> "июля"
-            "08" -> "августа"
-            "09" -> "сентября"
-            "10" -> "октября"
-            "11" -> "ноября"
-            "12" -> "декабря"
-            else -> ""
-        }
-        if (month == "" || daysInMonth(date[1].toInt(), date[2].toInt()) < day) return ""
-        return String.format("%d %s %s", day, month, date[2])
-    } catch (e: Exception) {
-        return ""
+    val date = digital.split(".")
+    if (date.size != 3 || date[0].toIntOrNull() == null || date[2].toIntOrNull() == null || date[1].toIntOrNull() == null) return ""
+    val day = date[0].toInt()
+    val month = when (date[1]) {
+        "01" -> "января"
+        "02" -> "февраля"
+        "03" -> "марта"
+        "04" -> "апреля"
+        "05" -> "мая"
+        "06" -> "июня"
+        "07" -> "июля"
+        "08" -> "августа"
+        "09" -> "сентября"
+        "10" -> "октября"
+        "11" -> "ноября"
+        "12" -> "декабря"
+        else -> ""
     }
+    if (month == "" || daysInMonth(date[1].toInt(), date[2].toInt()) < day) return ""
+    return String.format("%d %s %s", day, month, date[2])
 }
 
 /**
