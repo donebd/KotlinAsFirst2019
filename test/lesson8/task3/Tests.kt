c2bbabeed03b65bd2b9b389121a9b704fc51f594
done_bd <dinil.bd@mail.ru>
2019-11-16 13:02:07 +0300

    8 Lesson chess complete by use graph

--- test/lesson8/task3/Tests.kt
+++ test/lesson8/task3/Tests.kt
@@ -8,34 +8,6 @@ class Tests {
 
     @Test
     @Tag("Example")
-    fun bfs() {
-        val graph = Graph()
-        graph.addVertex("A")
-        graph.addVertex("B")
-        graph.addVertex("C")
-        graph.addVertex("D")
-        graph.addVertex("E")
-        graph.addVertex("F")
-        graph.addVertex("G")
-        graph.addVertex("H")
-        graph.connect("A", "B")
-        graph.connect("B", "C")
-        graph.connect("B", "D")
-        graph.connect("C", "E")
-        graph.connect("D", "F")
-        graph.connect("C", "F")
-        graph.connect("G", "H")
-        assertEquals(0, graph.bfs("A", "A"))
-        assertEquals(3, graph.bfs("A", "F"))
-        assertEquals(2, graph.bfs("E", "F"))
-        assertEquals(3, graph.bfs("E", "D"))
-        assertEquals(1, graph.bfs("H", "G"))
-        assertEquals(-1, graph.bfs("H", "A"))
-    }
-
-
-    @Test
-    @Tag("Example")
     fun dfs() {
         val graph = Graph()
         graph.addVertex("A")
