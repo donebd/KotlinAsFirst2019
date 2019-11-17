d185ab15dbdabc6b466096532528d5198e2037fa
done_bd <dinil.bd@mail.ru>
2019-11-17 18:02:05 +0300

    First try 9 lesson

--- src/lesson9/task1/Matrix.kt
+++ src/lesson9/task1/Matrix.kt
@@ -41,32 +41,49 @@ interface Matrix<E> {
  * height = высота, width = ширина, e = чем заполнить элементы.
  * Бросить исключение IllegalArgumentException, если height или width <= 0.
  */
-fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> = TODO()
+fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> =
+    if (height > 0 && width > 0) MatrixImpl(height, width, e)
+    else throw IllegalArgumentException()
 
 /**
  * Средняя сложность
  *
  * Реализация интерфейса "матрица"
  */
-class MatrixImpl<E> : Matrix<E> {
-    override val height: Int = TODO()
+class MatrixImpl<E>(override val height: Int, override val width: Int, value: E) : Matrix<E> {
+    private val map = mutableMapOf<Cell, E>()
 
-    override val width: Int = TODO()
+    init {
+        for (i in 1..height)
+            for (j in 1..width)
+                map[Cell(i, j)] = value
 
-    override fun get(row: Int, column: Int): E = TODO()
+    }
 
-    override fun get(cell: Cell): E = TODO()
+    override fun get(row: Int, column: Int): E = get(Cell(row, column))
+
+    override fun get(cell: Cell) = map[cell]!!
 
     override fun set(row: Int, column: Int, value: E) {
-        TODO()
+        set(Cell(row, column), value)
     }
 
     override fun set(cell: Cell, value: E) {
-        TODO()
+        map[cell] = value
     }
 
-    override fun equals(other: Any?) = TODO()
+    override fun equals(other: Any?) =
+        other is MatrixImpl<*> && height == other.height && width == other.width && map == other.map
 
-    override fun toString(): String = TODO()
+
+    override fun toString(): String = map.toString()
+
+    override fun hashCode(): Int {
+        var result = height
+        result = 31 * result + width
+        result = 31 * result + map.hashCode()
+        return result
+    }
+
 }
 
