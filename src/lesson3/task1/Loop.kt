c57f5068fa30c128f56146ddd36d2d47e2091d35
done_bd <dinil.bd@mail.ru>
2019-11-19 00:01:15 +0300

    Cleanup 3 lesson

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -131,16 +131,7 @@ fun minDivisor(n: Int): Int {
  *
  * Для заданного числа n > 1 найти максимальный делитель, меньший n
  */
-fun maxDivisor(n: Int): Int {
-    var answer = 0
-    for (i in n / 2 downTo 1) {
-        if (n % i == 0) {
-            answer = i
-            break
-        }
-    }
-    return answer
-}
+fun maxDivisor(n: Int): Int = n / minDivisor(n)
 
 /**
  * Простая
